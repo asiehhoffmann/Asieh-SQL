68a4dec8c68b1a1a89301058751b9a3aadd86ac5
Asieh Hoffmann <120616076+asiehhoffmann@users.noreply.github.com>
2023-01-16 16:42:34 -0700

    Create shoes_table.sql

---
+++ shoes_table.sql
@@ -0,0 +1,24 @@
+/** Shoes
+Athletic 
+**/
+
+CREATE TABLE shoes (id INTEGER PRIMARY KEY, price INTEGER, brand TEXT, size INTEGER, quantity INTEGER);
+
+INSERT INTO shoes VALUES (1,200,"adidas",10,3);
+INSERT INTO shoes VALUES (2,150,"keen",7,20);
+INSERT INTO shoes VALUES (3,170,"Hoka",8,5);
+INSERT INTO shoes VALUES (4,250,"adidas",5,10);
+INSERT INTO shoes VALUES (7,300,"Eddie Bauer",10,12);
+INSERT INTO shoes VALUES (8,100,"FILA",9,20);
+INSERT INTO shoes VALUES (9,500,"New Balance",10,20);
+INSERT INTO shoes VALUES (10,200,"adidas",8,19);
+INSERT INTO shoes VALUES (11,100,"FILA",9,20);
+INSERT INTO shoes VALUES (12,500,"New Balance",10,20);
+INSERT INTO shoes VALUES (13,200,"adidas",8,13);
+INSERT INTO shoes VALUES (14,100,"FILA",9,10);
+INSERT INTO shoes VALUES (15,500,"New Balance",7,20);
+
+select * from shoes;
+
+
+
